53d55efc64e324ed65104f0acde013d0bf8cff30
semoleg1986 <semoleg1986@gmail.com>
2023-08-30 03:18:21 +0300

    feat: impl. meth1

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let res = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        let letter = expr.slice(i, i + 10);
+        if (letter == '**********') {
+            res += ' ';
+        } else {
+            let letterDotDash = '';
+            for (let k = 0; k < letter.length; k += 2) {
+                let binar = letter.slice(k, k + 2);
+                switch (binar) {
+                    case '10':
+                        letterDotDash += '.';
+                        break;
+                    case '11':
+                        letterDotDash += '-';
+                        break;
+                    default:
+                        break;
+                }
+            }
+            res += MORSE_TABLE[letterDotDash];
+        }
+    }
+   return res;
 }
 
 module.exports = {
